1952216d2d2fb05c6cd64ff2986ac40c3cbdba02
Bobizl <bobi_aytos@abv.bg>
2023-04-26 15:33:22 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String n = "";
-        WriteInFile w = new WriteInFile();
-        w.writeToFile(n);
-        System.out.println();
+    public static void main(String[] args) {
+      WriteInFile n = new WriteInFile();
+      n.writeToFile();
 
     }
 }
--- src/WriteInFile.java
+++ src/WriteInFile.java
@@ -1,17 +1,26 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.Dictionary;
 import java.util.Scanner;
 
 public class WriteInFile {
-    public void writeToFile(String n) throws IOException{
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter username");
-        n = sc.nextLine();
-       try {
-           FileWriter fw = new FileWriter("E:\\Projects Ultimate\\WordLearner\\Words.txt");
-       }catch (Throwable e){
-
-       }
+    public void writeToFile(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the words you want to input in the dictionary = ");
+        String outputFile = "Dictionary.txt";
+       try(BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile,true))){
+           String input;
+           while(true){
+               input = scanner.nextLine();
+               if("exit".equalsIgnoreCase(input)){
+                   break;
+               }
+               writer.write(input);
+               writer.newLine();
+           }
+           System.out.println("Words saved to " + outputFile);
+       }catch(IOException e){
+           System.out.println("Something wrong happened !");
+        }
     }
-
 }
