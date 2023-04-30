951d4f9761822e084bb7f9f64bfc30b45891e1c6
Bobizl <bobi_aytos@abv.bg>
2023-04-30 23:13:43 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,9 @@
-import java.io.IOException;
-import java.util.HashMap;
 
-public class Main {
-    public static void main(String[] args) {
-     WriteInFile n = new WriteInFile();
-    n.writeToFile();
-    QuizGame quiz = new QuizGame();
-    quiz.shuffle();
+ class Main {
+     public static void main(String[] args){
+         UsersInput user = new UsersInput();
+         user.inputUser();
 
 
-        /*try {
-            ReadFile q = new ReadFile();
-            q.readFromFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-*/
-    }
-}
+     }
+ }
--- src/UsersInput.java
+++ src/UsersInput.java
@@ -6,36 +6,45 @@ import java.util.Scanner;
 
 public class UsersInput {
     public void inputUser(){
+
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your user ID: ");
+        int userId = scanner.nextInt();
+        scanner.nextLine(); // Consume the newline character
+
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        String userName = scanner.nextLine();
 
         System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        String userAge = scanner.nextLine();
 
-        addUserToDatabase(name, age);
+        System.out.print("Enter your points: ");
+        String userPoints = scanner.nextLine();
+
+        addPlayerToDatabase(userId, userName, userAge, userPoints);
     }
-
-    void addUserToDatabase(String name, int age) {
-        String url = "jdbc:mysql://localhost:3306/player";
+    private static void addPlayerToDatabase(int userId, String userName, String userAge, String userPoints) {
+        String url = "jdbc:mysql://localhost:3306/competitors";
         String user = "root";
         String password = "123456";
 
-        String insertUserQuery = "INSERT INTO users (name, age)";
+        String insertPlayerQuery = "INSERT INTO player (User_id, User_name, User_age, User_points) VALUES (?, ?, ?, ?)";
 
         try (Connection connection = DriverManager.getConnection(url, user, password);
-             PreparedStatement statement = connection.prepareStatement(insertUserQuery)) {
+             PreparedStatement statement = connection.prepareStatement(insertPlayerQuery)) {
 
-            statement.setString(1, name);
-            statement.setInt(2, age);
+            statement.setInt(1, userId);
+            statement.setString(2, userName);
+            statement.setString(3, userAge);
+            statement.setString(4, userPoints);
 
             int rowsInserted = statement.executeUpdate();
 
             if (rowsInserted > 0) {
-                System.out.println("User added successfully!");
+                System.out.println("Player added successfully!");
             } else {
-                System.out.println("Failed to add user.");
+                System.out.println("Failed to add player.");
             }
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
