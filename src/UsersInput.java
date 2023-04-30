6311e557f843111b8002b7cf14027bf13c1ca966
Bobizl <bobi_aytos@abv.bg>
2023-04-30 22:22:31 +0300

    new commit

--- src/UsersInput.java
+++ src/UsersInput.java
@@ -1,30 +1,46 @@
-import javax.xml.transform.Result;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
-import java.sql.ResultSet;
 
 public class UsersInput {
     public void inputUser(){
-        Scanner sc = new Scanner(System.in);
-        try{
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/competitors", "root" , "123456");
-            Statement st = con.createStatement();
-            ResultSet resultSet = st.executeQuery("select * from player");
-            System.out.print("Enter user name = ");
-            String userName = sc.nextLine();
-            System.out.print("Enter user age = ");
-            int userAge = sc.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-            st.executeUpdate("insert into player values('"+userName+"', '"+userAge+"')" );
-            System.out.println("Values inserted Successfully");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("player"));
-            }
-        }
-        catch (Exception e) {
-             e.printStackTrace();
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
 
-        }
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        addUserToDatabase(name, age);
     }
 
+    void addUserToDatabase(String name, int age) {
+        String url = "jdbc:mysql://localhost:3306/player";
+        String user = "root";
+        String password = "123456";
+
+        String insertUserQuery = "INSERT INTO users (name, age)";
+
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(insertUserQuery)) {
+
+            statement.setString(1, name);
+            statement.setInt(2, age);
+
+            int rowsInserted = statement.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("User added successfully!");
+            } else {
+                System.out.println("Failed to add user.");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
+
