c98419fc9278d702abb86cbf860888826c57b869
Bobizl <bobi_aytos@abv.bg>
2023-05-03 15:57:08 +0300

    new commit

--- src/QuizGame.java
+++ src/QuizGame.java
@@ -1,7 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -27,34 +24,46 @@ public class QuizGame {
 
         List<String> shuffledKeys = new ArrayList<>(dictionary.keySet());
         Collections.shuffle(shuffledKeys);
-
         Scanner scanner = new Scanner(System.in);
         int score = 0;
         for (String key : shuffledKeys) {
             System.out.println("What is the word for the following meaning?");
             System.out.println(dictionary.get(key));
             String userAnswer = scanner.nextLine().toLowerCase();
+            String selectLastInsertedUserIdQuery = "SELECT MAX(User_id) AS LastUserId FROM player";
+            int lastInsertedUserId = -1;
+            String url = "jdbc:mysql://localhost:3306/competitors";
+            String user = "root";
+            String password = "123456";
+            try (Connection connection = DriverManager.getConnection(url, user, password);
+                 PreparedStatement statement = connection.prepareStatement(selectLastInsertedUserIdQuery);
+                 ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    lastInsertedUserId = resultSet.getInt("LastUserId");
+                }
+            } catch (SQLException e) {
+                System.out.println("Error: " + e.getMessage());
+                e.printStackTrace();
+            }
 
             if (userAnswer.equals(key)) {
                 System.out.println("Correct!");
                 score++;
-                String url = "jdbc:mysql://localhost:3306/competitors";
-                String user = "root";
-                String password = "123456";
-                String insertPointsQuery = String.format("UPDATE player SET User_points=%da WHERE User_id=1", score);
+                String insertPointsQuery = String.format("UPDATE player SET User_points=%d WHERE User_id=%d", score, lastInsertedUserId);
                 try (Connection connection = DriverManager.getConnection(url, user, password);
                      PreparedStatement statement = connection.prepareStatement(insertPointsQuery)) {
-                     statement.executeUpdate();
+                    statement.executeUpdate();
                 } catch (SQLException e) {
                     System.out.println("Error: " + e.getMessage());
                     e.printStackTrace();
                 }
+
             } else {
                 System.out.println("Incorrect. The correct word is: " + key);
-
             }
+            System.out.println("User_id for the current player: " + lastInsertedUserId);
+            System.out.println("Your score: " + score + "/" + dictionary.size());
         }
-        System.out.println("Your score: " + score + "/" + dictionary.size());
         scanner.close();
     }
 }
