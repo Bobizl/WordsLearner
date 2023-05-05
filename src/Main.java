0382603fc3457fe54fadf25f23ec32af71e3f244
Bobizl <bobi_aytos@abv.bg>
2023-05-05 12:42:06 +0300

    new commit

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,68 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner menu = new Scanner(System.in);
-        String stopGame = "stop";
 
-        System.out.print("1. Input a user\n" + "2. Input words and their meanings\n" +
-                "3. See what words you have in yout Dictionary\n" +
-                "4. Play the game\n" +
-                "5. Delete the rang list\n");
-        System.out.print("Choose your option ");
-        int options;
-        options = menu.nextInt();
+    class Main {
+        public static void main(String[] args) throws IOException {
+            Scanner scanner = new Scanner(System.in);
+            String input = "";
+            while (true) {
+                System.out.print("1. Input a user\n" + "2. Input words and their meanings\n" +
+                        "3. See what words you have in your Dictionary\n" +
+                        "4. Play the game\n" +
+                        "5. Delete the rang list\n" +
+                        "6. Exit the program\n");
+                System.out.print("Choose your option: ");
+                if (scanner.hasNextLine()) {
+                    input = scanner.nextLine();
+                } else {
+                    break;
+                }
+                if (input.equals("6")) {
+                    System.out.println(" The program is shutting down ");
+                    break;
+                }
+                switch (input) {
+                    case "1":
+                        System.out.println("Input a user");
+                        UsersInput.inputUser();
+                        System.out.println();
+                        break;
 
-        do {
-            switch (options) {
-                case 1:
-                    System.out.println(" Input a user ");
-                    UsersInput.inputUser();
-                    break;
-                case 2:
-                    System.out.println(" Input words and their meanings ");
-                    WriteInFile.writeToFile();
-                    break;
-                case 3:
-                    System.out.println(" See the words you have in your Dictionary ");
-                    ReadFile n = new ReadFile();
-                    n.readFromFile();
-                    break;
-                case 4:
-                    System.out.println(" Play the game ");
-                    QuizGame.shuffle();
-                    break;
-                case 5:
-                    System.out.println(" Reset the rang list ");
-                    ResetUserId.deleteAll();
-                    ResetUserId.reset();
-                    break;
+                    case "2":
+                        System.out.println("Input words and their meanings");
+                        WriteInFile.writeToFile();
+                        System.out.println();
+                        break;
+
+                    case "3":
+                        System.out.println("See the words you have in your Dictionary");
+                        ReadFile n = new ReadFile();
+                        n.readFromFile();
+                        System.out.println();
+                        break;
+
+                    case "4":
+                        System.out.println("Play the game");
+                        QuizGame.shuffle();
+                        System.out.println();
+                        break;
+
+                    case "5":
+                        System.out.println("Reset the rang list");
+                        ResetUserId.deleteAll();
+                        ResetUserId.reset();
+                        System.out.println();
+                        break;
+
+                    default:
+                        System.out.println("Invalid option. Please try again.");
+                        System.out.println();
+                        break;
+                }
             }
-            while (options == 0) ;
-        }while(false);
+            scanner.close();
+        }
     }
-}
+
+
